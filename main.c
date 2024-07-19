c50d0b879c73edc5e665c04cd6a540309d67f463
Elie Fidele Rijaniaina <erijania@c1r5s4.42antananarivo.mg>
2024-07-19 11:12:11 +0300

    init philosopher structure

--- main.c
+++ main.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosopher.h                                      :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/08 15:50:37 by erijania          #+#    #+#             */
-/*   Updated: 2024/07/19 09:35:23 by erijania         ###   ########.fr       */
+/*   Created: 2024/07/19 09:30:55 by erijania          #+#    #+#             */
+/*   Updated: 2024/07/19 09:30:55 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHER_H
-# define PHILOSOPHER_H
+#include "philosopher.h"
 
-enum
+int	main(int argc, char **argv)
 {
-	PHILO_THINKING = 0,
-	PHILO_EATIN = 1,
-	PHILO_SLEEPING = 2
-};
-typedef struct s_phylosopher	t_phylosopher;
-struct s_phylosopher
-{
-	int	rank;
-	int	state;
-
-};
-#endif
+	return (0);
+}
--- src/philosopher/ft_new.c
+++ src/philosopher/ft_new.c
@@ -1,29 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosopher.h                                      :+:      :+:    :+:   */
+/*   ft_new.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/08 15:50:37 by erijania          #+#    #+#             */
-/*   Updated: 2024/07/19 09:35:23 by erijania         ###   ########.fr       */
+/*   Created: 2024/07/19 09:30:51 by erijania          #+#    #+#             */
+/*   Updated: 2024/07/19 09:35:43 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHER_H
-# define PHILOSOPHER_H
+#include "philosopher.h"
+#include <stdlib.h>
 
-enum
+t_phylosopher	*ft_new_philosopher(int rank)
 {
-	PHILO_THINKING = 0,
-	PHILO_EATIN = 1,
-	PHILO_SLEEPING = 2
-};
-typedef struct s_phylosopher	t_phylosopher;
-struct s_phylosopher
-{
-	int	rank;
-	int	state;
+	t_phylosopher	*ft;
 
-};
-#endif
+	ft = (t_phylosopher *)malloc(sizeof(t_phylosopher));
+	if (!ft)
+		exit(1);
+	ft->rank = rank;
+	return (ft);
+}
