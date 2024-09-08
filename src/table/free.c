fe0eedb975ef9107fdd7fc569638a4d738722497
Rijaniaina Elie Fidèle <elierijaniaina@gmail.com>
2024-09-09 00:12:29 +0300

    check memory leak for forks

--- src/table/free.c
+++ src/table/free.c
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 15:10:33 by erijania          #+#    #+#             */
-/*   Updated: 2024/09/08 13:53:41 by erijania         ###   ########.fr       */
+/*   Updated: 2024/09/09 00:11:50 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	pl_free(t_table *tab)
 	{
 		pthread_mutex_unlock(&tab->forks[i]->lock);
 		pthread_mutex_destroy(&tab->forks[i]->lock);
+		free(tab->forks[i]);
 		i++;
 	}
 	i = 0;
