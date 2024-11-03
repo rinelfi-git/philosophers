3d000c8b1b0eac2ee42d304f3bb9bc70dde24500
Rijaniaina Elie Fidèle <elierijaniaina@gmail.com>
2024-11-03 22:42:49 +0300

    effacer le utimestamp

--- philo/src/utils/timestamp.c
+++ philo/src/utils/timestamp.c
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 17:09:39 by erijania          #+#    #+#             */
-/*   Updated: 2024/11/03 22:31:51 by erijania         ###   ########.fr       */
+/*   Updated: 2024/11/03 22:42:39 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,4 @@ long	pl_timestamp(void)
 
 	gettimeofday(&time, 0);
 	return (time.tv_sec * 1000 + time.tv_usec / 1000);
-}
-long	pl_utimestamp(void)
-{
-	struct timeval	time;
-
-	gettimeofday(&time, 0);
-	return (time.tv_sec * 1000000 + time.tv_usec);
 }
